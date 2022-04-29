67bead8ff84a5021785859a9cfbffe0ef18a9276
devayache <andersonayache@gmail.com>
2022-04-28 21:43:29 -0300

    delete

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.taskList, name='task-List'),
     path('task/<int:id>', views.taskview, name="task-view"),
     path('newtask/', views.newtask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
     path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
